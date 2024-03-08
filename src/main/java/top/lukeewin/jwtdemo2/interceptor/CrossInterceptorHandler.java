86959185fdaf38c10b532fab4680b6bb21f0c52e
Yuting Wang <yw4554@nyu.edu>
2024-03-08 15:00:51 -0600

    cross interceptor

--- src/main/java/top/lukeewin/jwtdemo2/interceptor/CrossInterceptorHandler.java
+++ src/main/java/top/lukeewin/jwtdemo2/interceptor/CrossInterceptorHandler.java
@@ -1,4 +1,16 @@
 package top.lukeewin.jwtdemo2.interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CrossInterceptorHandler {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET , PUT , OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,accept,authorization,content-type");
+        return true;
+    }
 }

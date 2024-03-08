ccb1554401775037e3a9e5c51debbad25564a102
Yuting Wang <yw4554@nyu.edu>
2024-03-08 12:25:51 -0600

    first init

---
+++ src/main/java/top/lukeewin/jwtdemo2/JwtDemo2Application.java
@@ -0,0 +1,13 @@
+package top.lukeewin.jwtdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/top/lukeewin/jwtdemo2/JwtDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lukeewin.jwtdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
